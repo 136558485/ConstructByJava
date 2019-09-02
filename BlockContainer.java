2c914124a58a33c1c925a27064fd4c8dd65a2027
136558485 <36109346+136558485@users.noreply.github.com>
2019-09-02 23:04:04 +0800

    BlockContainer.java
    生产者消费者模型

---
+++ BlockContainer.java
@@ -0,0 +1,60 @@
+package demo.container;
+
+import java.util.Arrays;
+
+public class BlockContainer<T> {
+	private T[] array;
+	private int size;
+	
+	public BlockContainer() {
+		this(3);
+	}
+	
+	public BlockContainer(int n) {
+		this.array = (T[])new Object[n];
+		this.size = n;
+	}
+	
+	//添加元素到队列
+	public synchronized void put(T t){
+		//当size为0，说明数组已经满了，此时线程阻塞
+		while(this.size == 0)
+			try {
+				this.wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		//基于先进先出原则，从前往后往数组添加元素
+		array[array.length-size] = t;
+		size--;
+		//唤醒消费者get元素
+		this.notify();
+		
+	}
+	
+	public synchronized T get() {
+		//当size为数组的长度，说明此时队列没有值，此时线程阻塞
+		while(this.size == array.length)
+			try {
+				this.wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		//基于先进先出原则，获取第一个元素
+		Object t = array[0];
+		//获取后，将数组后面的元素往前挪一个位置
+		System.arraycopy(array, 1, array, 0, array.length-1);
+		array[array.length-1] = null;
+		size++;
+		//唤醒生产者线程put元素
+		this.notify();
+		return (T)t;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "BlockContainer [array=" + Arrays.toString(array) + "]";
+	}
+
+}
---
+++ ContainerTest.java
@@ -0,0 +1,64 @@
+package demo.container;
+
+public class ContainerTest {
+
+	public static void main(String[] args) {
+		BlockContainer<String> container = new BlockContainer<String>();
+		//测试一 ：容器满时阻塞无法在生产数据进去
+//		container.put("张三");
+//		container.put("李四");
+//		container.put("王五");
+//		System.out.println("get:"+container.toString());
+//		container.put("赵六");
+//		System.out.println(container.toString());
+		
+		//测试二 ：容器没有数据时无法消费数据
+//		System.out.println(container.get());
+		
+		//测试三 ：一边生产一边消费
+		Producer p = new Producer(container);
+		Consumer c = new Consumer(container);		
+		p.start();
+		c.start();
+	}
+
+}
+
+class Producer extends Thread{
+		
+	private BlockContainer<Integer> container;
+	
+	public Producer(BlockContainer container) {
+		this.container = container;
+	}	
+	public void run() {
+		int i = 0;
+		while(true) {
+			container.put(++i);
+			System.out.println("put:"+container.toString());
+			try {
+				this.sleep(1000);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}		
+	}	
+}
+class Consumer extends Thread{
+	
+	private BlockContainer<Integer> container;
+	
+	public Consumer(BlockContainer container) {
+		this.container = container;
+	}	
+	public void run() {
+		while(true) {
+			System.out.println("get:"+container.get());
+			try {
+				this.sleep(3000);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}		
+	}	
+}
